371b458433c0b9ef39b69c3eee7084982a1fd318
stephann <3025661+stephannv@users.noreply.github.com>
2022-10-30 17:15:34 -0300

    config: Generators and UUID as primary key

---
+++ config/initializers/generators.rb
@@ -0,0 +1,6 @@
+Rails.application.config.generators do |g|
+  g.orm :active_record, primary_key_type: :uuid
+  g.factory_bot suffix: "factories"
+  g.fixture_replacement :factory_bot, dir: "spec/factories"
+  g.test_framework :rspec
+end
